a05f209470abc7eb747afbe78bd367dcc0120d41
weixin <yanmingxin@yanmingxindeiMac.local>
2016-06-21 11:33:51 +0800

    add snapkit

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
-
+import SnapKit
 class ViewController: UIViewController {
     var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView = UITableView()
+        tableView.backgroundColor = UIColor.grayColor()
         self.view.addSubview(tableView)
+        tableView.snp_makeConstraints { (make) in
+            make.edges.equalTo(self.view)
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
